2dcf92a56686e6ad36fe3108044d38125b6fddb4
iamravi <ravikumar.k@ninjacart.com>
2020-11-27 21:05:15 +0530

    Vendor files Added

---
+++ js/index.js
@@ -0,0 +1,48 @@
+!(function($) {
+  "use strict";
+
+  // Smooth scroll
+  $( '.nav-menu a.smooth-scroll' ).on( 'click', function(e){
+
+    var href = $(this).attr( 'href' );
+
+    // Run a scroll animation to the position of the element which has the same id like the href value.
+    $( 'html, body' ).animate({
+      scrollTop: $( href ).offset().top
+    }, '300' );
+
+    e.preventDefault();
+  });
+
+  $(".my-work-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    nav:true,
+    margin: 20,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      900: {
+        items: 3
+      }
+    }
+  });
+
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      once: true
+    });
+  }
+  $(window).on('load', function() {
+    aos_init();
+  });
+
+
+})(jQuery);
