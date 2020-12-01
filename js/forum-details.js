e8d11ce1aee5fc737dfdb81c648fe875a6369ea8
iamravi <ravikumar.k@ninjacart.com>
2020-12-01 09:41:32 +0530

    planhop and ninjacart added in Home

--- js/forum-details.js
+++ js/forum-details.js
@@ -14,6 +14,21 @@
     });
   });
 
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1200);
+    return false;
+  });
+
   // Init AOS
   function aos_init() {
     AOS.init({
